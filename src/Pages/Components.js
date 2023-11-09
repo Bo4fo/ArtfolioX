14afc51e01c8ee2d598ccf54dcbfe9cac713c5af
Boafo Philip <61985077+Bo4fo@users.noreply.github.com>
2023-11-09 16:01:01 +0000

    9th Nov on component form

--- src/Pages/Components.js
+++ src/Pages/Components.js
@@ -1,36 +1,189 @@
 import React from 'react'
 import '../Maincss/Components.css';
+import { FaFolderOpen, FaForumbee, FaFigma } from "react-icons/fa";
+import { PiPlugs } from "react-icons/pi";
+
+
 
 
 
 
 function Components() {
+  const HomeStyles = {
+    fontWeight: 400,
+    color: 'white',
+    fontSize: 45,
+    paddingLeft: '0.8%',
+  };
+  const FontStyles = {
+    marginTop: '-9%',
+    textAlign: "center",
+    paddingLeft: '2%',
+    fontSize: 15,
+    color: 'white',
+    textAlign: 'left'
+  }
+  const FontStyles1 = {
+    marginTop: '-15%',
+    textAlign: "center",
+    paddingLeft: '2%',
+    fontSize: 15,
+    color: 'white',
+    textAlign: 'left'
+  }
+  const moveLeft ={
+    paddingLeft: '1%',
+}
+
   return (
-    
+
     <div className='component-container'>
-  
-    <div className="grid-form">
-    <div className="grid-item">
-      <h1>Grid 1</h1>
+      <div className="component-header">
+        <div className='component-item-header'>
+          <span style={HomeStyles}>
+            Components
+          </span>
+          <p style={moveLeft}>
+            Explore the code component i have created for others.
+          </p>
+          <div className="grid-form">
+            <div className="grid-item">
+              <div className='icon-folder'>
+                <span className='icon-large'>
+                  <FaFolderOpen />
+                </span>
+                <p>
+                  <h4 style={FontStyles}>
+                    UI & UX
+                  </h4>
+                </p>
+                <p>
+                  <h4 style={FontStyles1}>
+                    Make any set of react component <br /> draggable
+
+                  </h4>
+                </p>
+                {/* <div className="inside-text">
+                  <span>Adding some magic to your website with this funky Component for figma. Component made by Boafo Philip and friends.
+                  </span>
+                </div> */}
+              </div>
+            </div>
+            <div className="grid-item">
+              <div className='icon-folder'>
+                <span className='icon-large'>
+                  <FaFolderOpen />
+                </span>
+                <p>
+                  <h4 style={FontStyles}>
+                    UI & UX
+                  </h4>
+                </p>
+                <p>
+                  <h4 style={FontStyles1}>
+                    Make any set of react component <br /> draggable
+
+                  </h4>
+                </p>
+                {/* <div className="inside-text">
+                  <span>Adding some magic to your website with this funky Component for figma. Component made by Boafo Philip and friends.
+                  </span>
+                </div> */}
+              </div>
+            </div>
+            <div className="grid-item">
+              <div className='icon-folder'>
+                <span className='icon-large'>
+                  <FaFolderOpen />
+                </span>
+                <p>
+                  <h4 style={FontStyles}>
+                    UI & UX
+                  </h4>
+                </p>
+                <p>
+                  <h4 style={FontStyles1}>
+                    Make any set of react component <br /> draggable
+
+                  </h4>
+                </p>
+                {/* <div className="inside-text">
+                  <span>Adding some magic to your website with this funky Component for figma. Component made by Boafo Philip and friends.
+                  </span>
+                </div> */}
+              </div>
+            </div>
+            <div className="grid-item">
+              <div className='icon-folder'>
+                <span className='icon-large'>
+                  <FaFolderOpen />
+                </span>
+                <p>
+                  <h4 style={FontStyles}>
+                    UI & UX
+                  </h4>
+                </p>
+                <p>
+                  <h4 style={FontStyles1}>
+                    Make any set of react component <br /> draggable
+
+                  </h4>
+                </p>
+                {/* <div className="inside-text">
+                  <span>Adding some magic to your website with this funky Component for figma. Component made by Boafo Philip and friends.
+                  </span>
+                </div> */}
+              </div>
+            </div>
+            <div className="grid-item">
+              <div className='icon-folder'>
+                <span className='icon-large'>
+                  <FaFolderOpen />
+                </span>
+                <p>
+                  <h4 style={FontStyles}>
+                    UI & UX
+                  </h4>
+                </p>
+                <p>
+                  <h4 style={FontStyles1}>
+                    Make any set of react component <br /> draggable
+
+                  </h4>
+                </p>
+                {/* <div className="inside-text">
+                  <span>Adding some magic to your website with this funky Component for figma. Component made by Boafo Philip and friends.
+                  </span>
+                </div> */}
+              </div>
+            </div>
+            <div className="grid-item">
+              <div className='icon-folder'>
+                <span className='icon-large'>
+                  <FaFolderOpen />
+                </span>
+                <p>
+                  <h4 style={FontStyles}>
+                    UI & UX
+                  </h4>
+                </p>
+                <p>
+                  <h4 style={FontStyles1}>
+                    Make any set of react component <br /> draggable
+
+                  </h4>
+                </p>
+                {/* <div className="inside-text">
+                  <span>Adding some magic to your website with this funky Component for figma. Component made by Boafo Philip and friends.
+                  </span>
+                </div> */}
+              </div>
+            </div>
+          </div>
         </div>
-    <div className="grid-item">
-      <h1>Grid 2</h1>
-        </div>
-    <div className="grid-item">
-      <h1>Grid 3</h1>
-        </div>
-    <div className="grid-item">
-      <h1>Grid 4</h1>
-        </div>
-    <div className="grid-item">
-      <h1>Grid 5</h1>
-        </div>
-    <div className="grid-item">
-      <h1>Grid 6</h1>
-        </div>
+      </div>
     </div>
-    </div>
-    
+
   )
 }
 
